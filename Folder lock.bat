93f4f4380c809b6f6cefdde49da739e1d31b379d
Himanshu Yadav <56100874+KratoSkills@users.noreply.github.com>
2025-07-12 17:56:57 +0530

    Invisible Input
    Password and Choices are now Invisible

--- Folder lock.bat
+++ Folder lock.bat
@@ -1,37 +1,47 @@
-Check the signs (more than)
-
-cls
 @ECHO OFF
-title Folder KratoSkills
-if EXIST "HTG Locker" goto UNLOCK
-if NOT EXIST KratoSkills goto MDLOCKER
+cls
+color 0A
+title Folder "KratoSkills"
+
+:: Failsafe: create if both folders are missing
+if NOT EXIST "krX-lock" if NOT EXIST "KratoSkills" goto MDLOCKER
+
+:: Main flow
+if EXIST "krX-lock" goto UNLOCK
+if NOT EXIST "KratoSkills" goto MDLOCKER
+
 :CONFIRM
-echo Are you sure you want to lock the folder(Y/N)
-set/p "cho=(more than)"
-if %cho%==Y goto LOCK
-if %cho%==y goto LOCK
-if %cho%==n goto END
-if %cho%==N goto END
+echo Are you sure you want to lock the folder (Y/N)?
+for /f "delims=" %%p in ('powershell -Command "$cho = Read-Host -AsSecureString 'Your Choice'; [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($cho))"') do set "cho=%%p"
+if /I "%cho%"=="Y" goto LOCK
+if /I "%cho%"=="N" goto END
 echo Invalid choice.
 goto CONFIRM
+
 :LOCK
-ren KratoSkills "HTG Locker"
-attrib +h +s "HTG Locker"
+ren "KratoSkills" "krX-lock"
+attrib +h +s "krX-lock"
 echo Folder locked
-goto End
+goto END
+
 :UNLOCK
-echo Enter password to unlock folder
-set/p "pass=(more than)"
-if NOT %pass%== H4ck3r goto FAIL
-attrib -h -s "HTG Locker"
-ren "HTG Locker" KratoSkills
-echo Folder Unlocked successfully
-goto End
+echo Enter password to unlock folder:
+for /f "delims=" %%p in ('powershell -Command "$pass = Read-Host -AsSecureString 'Password'; [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($pass))"') do set "pass=%%p"
+if NOT "%pass%"=="H4ck3r" goto FAIL
+attrib -h -s "krX-lock"
+ren "krX-lock" "KratoSkills"
+echo Folder unlocked successfully
+goto END
+
 :FAIL
-echo Invalid password
-goto end
+echo Invalid password. Exiting...
+timeout /t 1 >nul
+goto END
+
 :MDLOCKER
-md KratoSkills
-echo KratoSkills created successfully
-goto End
-:End
+md "KratoSkills"
+echo Folder '"KratoSkills"' created successfully
+goto END
+
+:END
+exit
